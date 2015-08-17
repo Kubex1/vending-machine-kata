313b6287650ae487afae101985c287618f7b60ca
Kubex1 <kubex321@gmail.com>
2015-08-17 16:07:47 +0200

    Start - displaying initial message

--- src/main/java/tdd/vendingMachine/VendingMachine.java
+++ src/main/java/tdd/vendingMachine/VendingMachine.java
@@ -2,4 +2,9 @@ package tdd.vendingMachine;
 
 public class VendingMachine {
 
+    public static final String INITIAL_DISPLAY_TEXT = "Select Product";
+
+    public String getDisplayText() {
+        return INITIAL_DISPLAY_TEXT;
+    }
 }
--- src/test/java/tdd/vendingMachine/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/VendingMachineTest.java
@@ -1,5 +1,23 @@
 package tdd.vendingMachine;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class VendingMachineTest {
 
+    private VendingMachine vendingMachine;
+
+    @Before
+    public void setUp() throws Exception {
+        vendingMachine = new VendingMachine();
+    }
+
+    @Test
+    public void testMachineDisplaysInitialDisplayText() throws Exception {
+        String initialDisplayText = vendingMachine.getDisplayText();
+
+        assertThat(initialDisplayText).isEqualTo(VendingMachine.INITIAL_DISPLAY_TEXT);
+    }
 }
